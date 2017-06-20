39a04e393039c737429b61248bc4a1d844ebd871
yoninachmany <yoninachmany@gmail.com>
2017-06-20 15:46:26 -0400

    >1 raster for testing

--- scripts/load_development_data.sh
+++ scripts/load_development_data.sh
@@ -36,8 +36,20 @@ then
         usage
     else
         download_if_not_exits \
-            "s3://raster-vision/datasets/spacenet/AOI_3_Paris/AOI_3_Paris_Train/MUL/MUL_AOI_3_Paris_img10.tif" \
-            "paris.tif"
+            "s3://raster-vision/datasets/spacenet/AOI_3_Paris/AOI_3_Paris_Train/MUL/MUL_AOI_3_Paris_img3.tif" \
+            "paris3.tif"
+        download_if_not_exits \
+            "s3://raster-vision/datasets/spacenet/AOI_3_Paris/AOI_3_Paris_Train/MUL/MUL_AOI_3_Paris_img4.tif" \
+            "paris4.tif"
+        download_if_not_exits \
+            "s3://raster-vision/datasets/spacenet/AOI_3_Paris/AOI_3_Paris_Train/MUL/MUL_AOI_3_Paris_img5.tif" \
+            "paris5.tif"
+        download_if_not_exits \
+            "s3://raster-vision/datasets/spacenet/AOI_3_Paris/AOI_3_Paris_Train/MUL/MUL_AOI_3_Paris_img6.tif" \
+            "paris6.tif"
+        download_if_not_exits \
+            "s3://raster-vision/datasets/spacenet/AOI_3_Paris/AOI_3_Paris_Train/MUL/MUL_AOI_3_Paris_img8.tif" \
+            "paris8.tif"
     fi
     exit
 fi
