d9c18b5382c86a818013944ca2847d99e669676d
yoninachmany <yoninachmany@gmail.com>
2017-06-22 11:58:13 -0400

    deployment bootstrap required update, based off creating EMR cluster

--- deployment/emr/bootstrap-geopyspark.sh
+++ deployment/emr/bootstrap-geopyspark.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # This scripts bootstraps each node in the the EMR cluster to install GeoPySpark
-
+sudo yum install geos-devel -y
 sudo pip-3.4 install geopyspark
-sudo geopyspark install-jar
+sudo touch /usr/local/lib/python3.4/site-packages/geopyspark/command/geopyspark.conf
+sudo chmod 777 /usr/local/lib/python3.4/site-packages/geopyspark/command/geopyspark.conf
+sudo touch /usr/local/lib/python3.4/site-packages/geopyspark/jars/geotrellis-backend-assembly-0.1.0.jar
+sudo chmod 777 /usr/local/lib/python3.4/site-packages/geopyspark/jars/geotrellis-backend-assembly-0.1.0.jar
+geopyspark install-jar
