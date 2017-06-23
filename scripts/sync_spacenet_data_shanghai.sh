b7ba285b448e6a23a608f654d4088b0503954613
yoninachmany <yoninachmany@gmail.com>
2017-06-23 11:51:10 -0400

    indent more

--- scripts/sync_spacenet_data_shanghai.sh
+++ scripts/sync_spacenet_data_shanghai.sh
@@ -17,18 +17,18 @@ Syncs Rio data from s3://spacenet-data to dest_s3_path, for EMR ingest.
 DIR="$(dirname "$0")"
 
 function download_shanghai() {
-  echo "==> Making local Shanghai data directory"
-  mkdir AOI_4_Shanghai
-  echo "==> Getting Shanghai data from s3://spacenet-dataset locally"
-  aws s3api get-object --bucket spacenet-dataset --key AOI_4_Shanghai/AOI_4_Shanghai_Train.tar.gz --request-payer requester AOI_4_Shanghai/AOI_4_Shanghai_Train.tar.gz
-  echo "==> Unzipping Shanghai data locally"
-  tar -xzf AOI_4_Shanghai/AOI_4_Shanghai_Train.tar.gz
-  echo "==> Deleting local zip files before copying local Shanghai data to s3"
-  find . -name "*.tar.gz" -type f -delete
-  echo "==> Copying Shanghai data to your s3 bucket"
-  aws s3 cp AOI_4_Shanghai/ s3://raster-vision/datasets/spacenet/AOI_4_Shanghai/ --recursive --exclude ".*"
-  echo "==> Removing Shanghai data locally"
-  rm -rf AOI_4_Shanghai
+    echo "==> Making local Shanghai data directory"
+    mkdir AOI_4_Shanghai
+    echo "==> Getting Shanghai data from s3://spacenet-dataset locally"
+    aws s3api get-object --bucket spacenet-dataset --key AOI_4_Shanghai/AOI_4_Shanghai_Train.tar.gz --request-payer requester AOI_4_Shanghai/AOI_4_Shanghai_Train.tar.gz
+    echo "==> Unzipping Shanghai data locally"
+    tar -xzf AOI_4_Shanghai/AOI_4_Shanghai_Train.tar.gz
+    echo "==> Deleting local zip files before copying local Shanghai data to s3"
+    find . -name "*.tar.gz" -type f -delete
+    echo "==> Copying Shanghai data to your s3 bucket"
+    aws s3 cp AOI_4_Shanghai/ s3://raster-vision/datasets/spacenet/AOI_4_Shanghai/ --recursive --exclude ".*"
+    echo "==> Removing Shanghai data locally"
+    rm -rf AOI_4_Shanghai
 }
 
 if [ "${BASH_SOURCE[0]}" = "${0}" ]
--- scripts/sync_spacenet_data_vegas.sh
+++ scripts/sync_spacenet_data_vegas.sh
@@ -17,18 +17,18 @@ Syncs Rio data from s3://spacenet-data to dest_s3_path, for EMR ingest.
 DIR="$(dirname "$0")"
 
 function download_vegas() {
-  echo "==> Making local Vegas data directory"
-  mkdir AOI_2_Vegas
-  echo "==> Getting Vegas data from s3://spacenet-dataset locally"
-  aws s3api get-object --bucket spacenet-dataset --key AOI_2_Vegas/AOI_2_Vegas_Train.tar.gz --request-payer requester AOI_2_Vegas/AOI_2_Vegas_Train.tar.gz
-  echo "==> Unzipping Vegas data locally"
-  tar -xzf AOI_2_Vegas/AOI_2_Vegas_Train.tar.gz
-  echo "==> Deleting local zip files before copying local Vegas data data to s3"
-  find . -name "*.tar.gz" -type f -delete
-  echo "==> Copying Vegas data to your s3 bucket"
-  aws s3 cp AOI_2_Vegas/ s3://raster-vision/datasets/spacenet/AOI_2_Vegas/ --recursive --exclude ".*"
-  echo "==> Removing Vegas data locally"
-  rm -rf AOI_2_Vegas
+    echo "==> Making local Vegas data directory"
+    mkdir AOI_2_Vegas
+    echo "==> Getting Vegas data from s3://spacenet-dataset locally"
+    aws s3api get-object --bucket spacenet-dataset --key AOI_2_Vegas/AOI_2_Vegas_Train.tar.gz --request-payer requester AOI_2_Vegas/AOI_2_Vegas_Train.tar.gz
+    echo "==> Unzipping Vegas data locally"
+    tar -xzf AOI_2_Vegas/AOI_2_Vegas_Train.tar.gz
+    echo "==> Deleting local zip files before copying local Vegas data data to s3"
+    find . -name "*.tar.gz" -type f -delete
+    echo "==> Copying Vegas data to your s3 bucket"
+    aws s3 cp AOI_2_Vegas/ s3://raster-vision/datasets/spacenet/AOI_2_Vegas/ --recursive --exclude ".*"
+    echo "==> Removing Vegas data locally"
+    rm -rf AOI_2_Vegas
 }
 
 if [ "${BASH_SOURCE[0]}" = "${0}" ]
