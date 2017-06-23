2cd27f7e624f0974fa62e09c1ff32cd5b86d617e
yoninachmany <yoninachmany@gmail.com>
2017-06-23 11:44:35 -0400

    instances back to m3.2xlarge

--- config-emr.mk
+++ config-emr.mk
@@ -1,7 +1,7 @@
 export NAME := SpaceNet Ingest ${USER}
-export MASTER_INSTANCE:=m3.xlarge
+export MASTER_INSTANCE:=m3.2xlarge
 export MASTER_PRICE := 0.5
-export WORKER_INSTANCE:=m3.xlarge
+export WORKER_INSTANCE:=m3.2xlarge
 export WORKER_PRICE := 0.5
 export WORKER_COUNT := 1
 export USE_SPOT := true
