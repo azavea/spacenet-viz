bfb206e96dd28cddd75504203910cef66fc8ee3a
yoninachmany <yoninachmany@gmail.com>
2017-06-27 16:29:09 -0400

    new center with enough adj tiles and higher zoom for specificity and basemap

--- src/app-frontend/js/common/constants.js
+++ src/app-frontend/js/common/constants.js
@@ -1,6 +1,6 @@
 export const isDevelopment = process.env.NODE_ENV === 'development';
 export const isProduction = process.env.NODE_ENV === 'production';
-export const defaultMapCenter = { lat: 49.045040, lng: 2.190852 };
+export const defaultMapCenter = { lat: 48.993174, lng: 2.271811 };
 
 export const LayerNames = {
     snowOn : "isprs-potsdam-dsm",
